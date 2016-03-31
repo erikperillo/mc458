7acf31dddd9c1c23830191eaaff1f38fac6a98e2
Erik Perillo <erik.perillo@gmail.com>
2016-03-31 20:02:54 -0300

    test updated

--- test.sh
+++ test.sh
@@ -10,11 +10,24 @@ cd $dir
 
 make
 
+out=$(mktemp)
+yes=0
+no=0
 for test_case in tests/*.in; do
-	echo "--- in $test_case.in ---"
+	echo "--- in $test_case ---"
+
+	expected=$(echo $test_case | rev | cut -f1 -d. --complement | rev).res
+
 	echo "program output:"
-	./$(basename $program) < $test_case
+	{ ./$(basename $program) < $test_case; } &> $out
+	cat -- $out
+
+	[[ -z "$(diff $out $expected)" ]] && yes=$((yes+1)) || no=$((no+1))
+
 	echo "expected output:"
-	cat $(echo $test_case | rev | cut -f1 -d. --complement | rev).res
+	cat $expected
 	echo
 done
+
+echo "correct results: $yes"
+echo "incorrect results: $no"
