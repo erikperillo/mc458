778437a898b719f9fb1a7e2eb4f635fa7366d0f3
Erik Perillo <erik.perillo@gmail.com>
2016-03-31 03:02:42 -0300

    test script added

---
+++ test.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+dir=$1
+
+[ -z "$dir" ] && { echo "invalid args"; exit 1; }
+
+program=$dir
+
+cd $dir
+for test_case in tests/*.in; do
+	echo "--- in $test_case.in ---"
+	echo "program output:"
+	./$(basename $program) < $test_case
+	echo "expected output:"
+	cat $(echo $test_case | rev | cut -f1 -d. --complement | rev).res
+	echo
+done
